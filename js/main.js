dfab1cb04aba08ed3c5ce06cf6d476fa4ff1efba
taniacoder <tania.cuyamedina@gmail.com>
2016-03-15 22:57:39 -0500

    kane

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.barra-nivel').each(function() {
+  var valorLargura = $(this).data('nivel');
+  var valorNivel = $(this).html("<span 
+ class='valor-nivel'>"+valorLargura+"</span>");
+    $(this).animate({
+        width: valorLargura
+    });
+});
